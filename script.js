5c86f457ccd4a605401aa701c1a029f65583636d
Sukanya Patra <sukanyapatraa@gmail.com>
2025-09-09 23:46:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function isPalindrome(str){
+
+    const cleanstr = str.toLowerCase().replace(/[^a-z0-9]/g,'');
+    const reversedstr = cleanstr.split('').reverse().join('');
+    return cleanstr === reversedstr;
+}
+
+
+function palindromeChecker(){
+
+    const inputText = document.getElementById('inputText');
+    const result = document.getElementById('result');
+    if(isPalindrome(inputText.value)){
+        result.textContent=`"${inputText.value}" is a Palindrome`;
+    }
+    else
+    {
+        result.textContent=`"${inputText.value}" is not a Palindrome`;
+    }
+
+    result.classList.add('fadeIn');
+    inputText.value='';
+
+}
+
+
+
+
+document.getElementById('checkButton').addEventListener("click",palindromeChecker);
